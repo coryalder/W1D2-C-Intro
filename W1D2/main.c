32b94f5c3766e413fc3ae7fa3f34bcf2b801acf9
Cory Alder <coryalder@gmail.com>
2015-06-08 21:07:28 -0400

    Initial Commit

---
+++ W1D2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  W1D2
+//
+//  Created by Cory Alder on 2015-06-08.
+//  Copyright (c) 2015 Cory Alder. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
